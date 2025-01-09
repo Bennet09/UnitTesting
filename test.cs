912adc9435a789d640ba57d10818075585e1416c
Bennet Mogale Ramphisa <101468367+Bennet09@users.noreply.github.com>
2025-01-09 12:09:37 +0200

    Add files via upload

---
+++ test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesting
+{
+    internal class test
+    {
+        private const int LIMIT = -50;
+        private int limitBalance;
+
+        public int LimitBalance { get { return limitBalance; } set { limitBalance = value; } }
+
+        public void checkLimit(int limit)
+        {
+            if (limit < 0)
+            {
+                throw new ArgumentOutOfRangeException("limit", limit, "The value was less");
+            }
+            if (limit > LIMIT)
+            {
+                throw new ArgumentOutOfRangeException("limit");
+            }
+
+        }
+    }
+}
